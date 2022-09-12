17698c66b4476e15da25b6f69b68ab51ff413654
devancakra <mudrawijaya@gmail.com>
2022-09-12 11:17:48 +0700

    upd: SQL Last Working

--- public/db_sql/stcamp404.sql
+++ public/db_sql/stcamp404.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Sep 2022 pada 03.03
+-- Waktu pembuatan: 12 Sep 2022 pada 11.15
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.4.15
 
@@ -39,14 +39,9 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_09_01_103906_create_siswa_table', 1),
-(6, '2022_09_10_002925_create_pelatihan_table', 1),
-(7, '2022_09_10_031445_create_siswa_table', 2),
-(8, '2022_09_12_024026_create_password_resets_table', 3),
-(9, '2022_09_12_025510_create_password_resets_table', 4);
+(2, '2022_09_10_002925_create_pelatihan_table', 1),
+(3, '2022_09_10_031445_create_siswa_table', 2),
+(4, '2022_09_12_025510_create_password_resets_table', 4);
 
 -- --------------------------------------------------------
 
@@ -61,13 +56,6 @@ CREATE TABLE `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `password_resets`
---
-
-INSERT INTO `password_resets` (`id`, `email`, `token`, `created_at`, `updated_at`) VALUES
-(1, '20220101@stcamp404.ac.id', '$2y$10$cP39Mza60ZPKzwBQInOGDeryxJNBZn65KJndjpiPHG6Z059SuwjTe', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -146,10 +134,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `siswa_id`, `name`, `role`, `email`, `image`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, NULL, 'Anastasya Geralda', 'admin', 'admin@stcamp404.ac.id', 'asset\\img\\profile\\admin.jpg', '$2y$10$sTFrUMwJgFADRIhmbqHJBe5Mqog2KAGDYJB9hqLjmohcmg5tcr7tO', NULL, NULL, NULL),
 (2, 20220101, 'Jaya Mangunati', 'siswa', '20220101@stcamp404.ac.id', 'asset\\img\\profile\\siswa.jpg', '$2y$10$27FeEhw9YVfZa3QyFrtl6OGFcxiu/6bTdNn5DUYWA4TxULmJosNJa', NULL, NULL, NULL),
-(4, 20220102, 'Jadiyan Marto', 'siswa', '20220102@stcamp404.ac.id', 'asset\\img\\profile\\default.jpg', '$2y$10$JjMoe9xh6nwaAHU4RmyqUOZVzSoxJJAF622jTvZlKDlWHxoA7FLFa', NULL, NULL, NULL),
-(5, 20220103, 'Chondro Aminoto', 'siswa', '20220103@stcamp404.ac.id', 'asset\\img\\profile\\default.jpg', '$2y$10$CPySEc5iR6htOJoMQUCdL.MJ56OPXpWanH95AKjVuxk5DslH7aJi.', NULL, NULL, NULL),
-(6, 20220104, 'Gatot Subroto', 'siswa', '20220104@stcamp404.ac.id', 'asset\\img\\profile\\default.jpg', '$2y$10$bVgjoiU5Jl3afVjEBtDWtuA7a01jVTZQ4qT2cGKsRhtyPeFt80XlG', NULL, NULL, NULL),
-(7, 20220105, 'Jihan Minarti', 'siswa', '20220105@stcamp404.ac.id', 'asset\\img\\profile\\default.jpg', '$2y$10$Q2Twy2fd0gtl66MFg6icC.x1WPRnj5WwBXlIJCXRawxrZZ70hKOMO', NULL, NULL, NULL);
+(3, 20220102, 'Jadiyan Marto', 'siswa', '20220102@stcamp404.ac.id', 'asset\\img\\profile\\default.jpg', '$2y$10$TwCx6BaKxr01JNkp1ja0g.siqqnnvDh14.qGarrOKEOGp2aT5GZVG', NULL, NULL, '2022-09-12 04:08:10');
 
 --
 -- Indexes for dumped tables
@@ -195,13 +180,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `pelatihan`
@@ -219,7 +204,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
